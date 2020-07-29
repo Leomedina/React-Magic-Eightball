f6e15418bd4850512ddf86b4487d85e29a3b0ec9
Leo <medinayleo@gmail.com>
2020-07-29 15:03:12 -0400

    initial commit

---
+++ src/Eightball.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import DefaultAnswers from 'DefaultAnswers';
+
+
+const EightBall = ({ answers = DefaultAnswers }) => (
+  <>
+  null;
+  </>
+);
+
+export default Eightball;

e19515e681b7eafa7e36398bfe49e9729a50df44
Leo <medinayleo@gmail.com>
2020-07-30 11:01:02 -0400

    completed project

--- src/Eightball.js
+++ src/Eightball.js
@@ -5,13 +5,18 @@ function randomNum(size) {
   return Math.floor(Math.random() * size)
 }
 
-function EightBall({ answers = DefaultAnswers }) {
-  const [answer, setAnswer] = useState("Think of a question");
 
+const EightBall = ({ answers = DefaultAnswers }) => {
+  const [answer, setAnswer] = useState("Think of a question");
+  const [state, setState] = useState("black");
   return (
-    <div className="Ball">
+    <div className="Ball" style={{ backgroundColor: `${state}` }} onClick={() => {
+      const newIndex = answers[randomNum(answers.length)]
+      setAnswer(newIndex.msg);
+      setState(newIndex.color);
+    }}>
       <p>{answer}</p>
-    </div>
+    </div >
   )
 }
 
